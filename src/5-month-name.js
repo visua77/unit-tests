944b5ae512b0f7ac426fb6ded73571640d313161
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-12-27 16:15:13 +0100

    fixed 1 thru 5

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,18 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+
+
+    if (monthNumber > 12) {
+        return null
+    }
+
+    if (monthNumber < 1) {
+        return null
+    }
+
+    return months[monthNumber - 1]
 }
