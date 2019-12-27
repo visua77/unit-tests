4a456bd9b2597837ce5688b15535377616977b02
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-12-27 18:03:07 +0100

    fixed no8

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,16 @@
 export const hackerSpeak = (words) => {
-  return words
+
+    words = words.replace(/a/g, "4")
+    words = words.replace(/e/g, "3")
+    words = words.replace(/i/g, "1")
+    words = words.replace(/o/g, "0")
+    words = words.replace(/s/g, "5")
+
+    words = words.replace(/A/g, "4")
+    words = words.replace(/E/g, "3")
+    words = words.replace(/I/g, "1")
+    words = words.replace(/O/g, "0")
+    words = words.replace(/S/g, "5")
+    return words
+
 }
